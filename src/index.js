f495181816924f68df8de2ab3c1398da70a55755
Azat Shagabiev <azatsagabiev32@gmail.com>
2026-06-05 22:42:41 +0300

    Уникальные идентификаторы

--- src/index.js
+++ src/index.js
@@ -12,25 +12,12 @@ function getRandom() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Какое-то значение</h1>
-    <h1>{firstName} {lastName}</h1>
+    <h1 className='h1-style'>Какое-то значение</h1>
+    <h1 className='h1-style'>{firstName} {lastName}</h1>
     <ul>
-      {/* <li>Элемент 1</li>
-      <li>Элемент 2</li>
-      <li>Элемент 3</li>
-      <li>Элемент 4</li>
-      <li>Элемент 5</li>
-      <li>Элемент 6</li> */}
-      {/* {elements.map(el => <li>{`Элемент ${el}`}</li>)} //темка нереальная */}
       {Array.from({ length: 5 }, (_, item) =>
-        <li>{`Элемент ${getRandom() % 2 === 0 ? 'EVEN' : 'ODD'}`}</li>)}
+        <li key={item}>{`Элемент ${getRandom() % 2 === 0 ? 'EVEN' : 'ODD'}`}</li>)}
     </ul>
     <p>© Авторские права мои {currentYear}</p>
-    {/* <p>© Авторские права мои {new.Date().getFull;Year()}</p> */}
   </div>
 );
-//как можно было написать
-// const h1Element = document.createElement("h1");
-// h1Element.innerText = "Hello World!";
-// const root = document.getElementById("root");
-// root.appendChild(h1Element);
